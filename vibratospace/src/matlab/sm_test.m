63e1c6bafa96199689df7d933aed8f17f9c2d4d3
maxsolomonhenry <max.henry@mail.mcgill.ca>
2021-02-09 19:30:22 -0500

    Debug

--- vibratospace/src/matlab/sm_test.m
+++ vibratospace/src/matlab/sm_test.m
@@ -1,21 +1,23 @@
 % Test the wrapper.
+clearvars;
 
-PATH_TO_DATA = '/Users/maxsolomonhenry/Documents/Python/vibrato_space/vibratospace/data/world_data.pickle';
+PATH_TO_DATA = '/Users/maxsolomonhenry/Documents/Python/vibrato_space/vibratospace/data/data.pickle';
 
 fid = py.open(PATH_TO_DATA,'rb');
 data = py.pickle.load(fid);
 
-wav = getWav(data, 1);
+[fs, wav] = getWav(data, 6);
 
 % Plot test and reconstruction.
-plot(sineModelResynthesis(wav, fs, 5));
+plot(sineModelResynthesis(wav, fs, 100));
 % hold on
 % plot(t, wav);
 xlabel('time (s)');
 ylabel('amplitude');
 % hold off;
 
-function wav = getWav(data, k)
+function [fs, wav] = getWav(data, k)
     temp = data(k);
-    wav = double(temp{1}{'frequency'});
+    wav = double(temp{1}{'crepe_f0'});
+    fs = double(temp{1}{'sample_rate'});
 end
